5a946f5b580af33e5e73e83f9e224482e9c9fcf0
skygarlics <parkjhon20@gmail.com>
2018-10-31 20:46:03 +0900

    support utf-8 playlist

--- mintube.cmd
+++ mintube.cmd
@@ -1,10 +1,17 @@
 @ECHO off
 setlocal
 
+rem set codepage to support utf-8
+CHCP 65001 > NUL
+
 set param=%~1
 
 IF "%param%" == "" (
-    set /p param="Enter Video ID or CSV file: "
+    IF exist ".\playlist.csv" (
+        set param=.\playlist.csv
+    ) ELSE (
+        set /p param="Enter Video ID or CSV file: "
+    )
 )
 IF "%param%" == "" (
     echo parameter is empty
@@ -22,11 +29,14 @@ exit /b
 
 
 :PLAYLIST
+echo Load %1%
+echo.
 set /a cnt=0
 set arr[0]=
 for /F "skip=1 tokens=1,2 usebackq delims=," %%A in ("%1") do (
     call :CNTUP "%%A" %%B
 )
+echo.
 :SELECT
 set /p channel="Select Channel: "
 call set vid=%%arr[%channel%]%%
@@ -36,7 +46,7 @@ exit /b
 
 :CNTUP
 set /a cnt=cnt+1
-echo|set /p=%cnt% 
+echo|set /p=%cnt% : 
 echo %1
 set arr[%cnt%]=%2
 exit /b
